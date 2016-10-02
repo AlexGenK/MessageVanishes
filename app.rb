80cdc5782bca5aa10b0b47161bd4e2381aa2884c
Oleksii Kozhyn <suburbakskin@gmail.com>
2016-10-02 23:59:04 +0300

    create database

--- app.rb
+++ app.rb
@@ -2,6 +2,11 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
 
+set :database, "sqlite3:vanishes.db"
+
+class Message < ActiveRecord::Base
+end
+
 get '/' do
   erb :message
 end
