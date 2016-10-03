2e064a66b652d9f7e3c02c771b82678b46fca511
Oleksii Kozhyn <suburbakskin@gmail.com>
2016-10-03 12:56:16 +0300

    store the messages

--- app.rb
+++ app.rb
@@ -1,12 +1,25 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
+require "securerandom"
 
 set :database, "sqlite3:vanishes.db"
 
 class Message < ActiveRecord::Base
 end
 
+def generate_link(size)
+  SecureRandom.urlsafe_base64(size, false)
+end
+
 get '/' do
+  @m=Message.new
   erb :message
+end
+
+post '/' do
+  @m=Message.new(params[:message])
+  @m.link=generate_link(11)
+  @m.save
+  erb :add
 end
