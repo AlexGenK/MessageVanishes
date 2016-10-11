6cb0f4fe85ee19fecccc7b6e423ff147a9e845c7
Oleksii Kozhyn <suburbakskin@gmail.com>
2016-10-11 21:42:47 +0300

    configure Shoulda Matchers and add Message model specs

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -3,6 +3,19 @@ ENV['RACK_ENV'] = 'test'
 require './app.rb'
 require 'rspec'
 require 'rack/test'
+require 'shoulda/matchers'
+
+RSpec.configure do |config|
+  config.include(Shoulda::Matchers::ActiveModel, type: :model)
+  config.include(Shoulda::Matchers::ActiveRecord, type: :model)
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :active_record
+  end
+end
 
 describe 'The MessageVanishes App' do
   include Rack::Test::Methods
@@ -16,4 +29,10 @@ describe 'The MessageVanishes App' do
     expect(last_response).to be_ok
     expect(last_response.body).to include('Enter your message')
   end
+end
+
+describe Message, type: :model do
+  it { should validate_presence_of(:link) }
+  it { should validate_presence_of(:method) }
+  it { should validate_presence_of(:count) }
 end
