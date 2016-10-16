76998a9be8ac3a1efbe3d3249943edc31868f9ff
Oleksii Kozhyn <suburbakskin@gmail.com>
2016-10-16 20:08:13 +0300

    fix decrypt_message function for Firefox

--- public/js/app.js
+++ public/js/app.js
@@ -12,6 +12,6 @@ function encrypt_message(message, password, target, error_field) {
 function decrypt_message(message, password, target) {
 	var d_m=CryptoJS.AES.decrypt($(message).val(),$(password).val()).toString(CryptoJS.enc.Utf8);
 	$(target).val(
-		 d_m ? d_m : "!!! Wrong password !!!"
+		 d_m=="" ? "!!! Wrong password !!!" : d_m
 		);
 }
